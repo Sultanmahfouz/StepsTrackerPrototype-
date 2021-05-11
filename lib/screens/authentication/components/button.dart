34078dec8240443685e4651ffee1bffdf3df0ec3
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-11 22:39:51 +0300

    finished building sign in UI

---
+++ lib/screens/authentication/components/button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:steps_tracker_prototype/components/constants.dart';
+
+// ignore: must_be_immutable
+class AuthButton extends StatelessWidget {
+  Function onPressed;
+  String title;
+
+  AuthButton({this.onPressed, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: 160,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        color: Colors.green,
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.grey[200],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          title,
+          style: TextStyle(
+            color: secondaryColor,
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/authentication/login.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:steps_tracker_prototype/components/constants.dart';
+
+import 'components/button.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String email;
+  String password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: decorationLayout,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 200, left: 25),
+                child: Text(
+                  'Sign in',
+                  style: textHeaderStyle,
+                ),
+              ),
+              Container(
+                padding:
+                    EdgeInsets.only(top: 40, left: 25, right: 25, bottom: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: fieldDecoration,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          password = value;
+                        });
+                      },
+                      obscureText: true,
+                      decoration: fieldDecoration.copyWith(
+                          labelText: 'Password',
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: primaryColor,
+                          )),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              'Forgot password',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Text(
+                            'Create an account?',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                child: AuthButton(
+                  onPressed: () {},
+                  title: 'Login',
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Center(
+                child: GestureDetector(
+                  onTap: () {
+                    print('Join as a guest');
+                  },
+                  child: Text(
+                    'Join as a guest ----> ',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

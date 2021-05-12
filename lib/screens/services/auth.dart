856f7ef8507c84211ed1d0082d1c18981d114492
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-12 17:22:40 +0300

    sign up a new user method

---
+++ lib/screens/services/auth.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:steps_tracker_prototype/models/user.dart';
+import 'package:steps_tracker_prototype/screens/services/user.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign up a user
+  Future<UserData> signUpWithEmailAndPassword(
+    String email,
+    String password,
+    String name,
+    String avatar,
+  ) async {
+    UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    User user = result.user;
+    await UserService(
+      uid: user.uid,
+      name: name,
+      avatar: avatar,
+    ).setUserData(
+      UserData(
+        uid: user.uid,
+        name: name,
+        avatar: avatar,
+      ),
+    );
+    return user != null
+        ? UserData(
+            uid: user.uid,
+            name: name,
+            avatar: avatar,
+          )
+        : null;
+  }
+}

55a837850e9269478c1ff2b0e68857199ba461b5
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-20 16:49:18 +0300

    build Reward list Card UI

---
+++ lib/screens/rewards/card/list.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:steps_tracker_prototype/models/reward.dart';
+
+import 'item.dart';
+
+// ignore: must_be_immutable
+class RewardListCard extends StatefulWidget {
+  List<Reward> allRewards;
+
+  RewardListCard({this.allRewards});
+
+  @override
+  _RewardListCardState createState() => _RewardListCardState();
+}
+
+class _RewardListCardState extends State<RewardListCard> {
+  @override
+  Widget build(BuildContext context) {
+    if (widget.allRewards != null) {
+      if (widget.allRewards.length != 0) {
+        return SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: List.generate(
+              widget.allRewards.length,
+              (index) => RewardItemCard(
+                reward: widget.allRewards[index],
+              ),
+            ),
+          ),
+        );
+      } else {
+        return Container(
+          padding: EdgeInsets.all(10),
+          child: Text('Unfortunatel, no rewards availabe at the moment :)'),
+        );
+      }
+    } else {
+      return Container(
+        padding: EdgeInsets.all(10),
+        child: Text('Unfortunatel, no rewards availabe at the moment :)'),
+      );
+    }
+  }
+}

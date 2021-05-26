59bf7c04495878b25b232747e577cb1ce3df1ef2
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-26 20:45:46 +0300

    fix: text coupons

--- lib/screens/coupons/card/list.dart
+++ lib/screens/coupons/card/list.dart
@@ -5,9 +5,9 @@ import 'item.dart';
 
 // ignore: must_be_immutable
 class CouponListCard extends StatefulWidget {
-  List<Reward> allRewards;
+  List<Reward> allCoupons;
 
-  CouponListCard({this.allRewards});
+  CouponListCard({this.allCoupons});
 
   @override
   _CouponListCardState createState() => _CouponListCardState();
@@ -16,15 +16,15 @@ class CouponListCard extends StatefulWidget {
 class _CouponListCardState extends State<CouponListCard> {
   @override
   Widget build(BuildContext context) {
-    if (widget.allRewards != null) {
-      if (widget.allRewards.length != 0) {
+    if (widget.allCoupons != null) {
+      if (widget.allCoupons.length != 0) {
         return SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: Column(
             children: List.generate(
-              widget.allRewards.length,
+              widget.allCoupons.length,
               (index) => CouponItemCard(
-                reward: widget.allRewards[index],
+                coupon: widget.allCoupons[index],
               ),
             ),
           ),
@@ -32,13 +32,13 @@ class _CouponListCardState extends State<CouponListCard> {
       } else {
         return Container(
           padding: EdgeInsets.all(10),
-          child: Text('Unfortunatel, no rewards availabe at the moment :)'),
+          child: Text('Unfortunately, no rewards availabe at the moment :)'),
         );
       }
     } else {
       return Container(
         padding: EdgeInsets.all(10),
-        child: Text('Unfortunatel, no rewards availabe at the moment :)'),
+        child: Text('Unfortunately, no rewards availabe at the moment :)'),
       );
     }
   }

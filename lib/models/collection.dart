7504cc0f76c06be7ca7c430b88a46e9010e60edb
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-26 10:53:35 +0300

    add reward collection to the model

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class FirebaseCollection {
+class FirebaseCollections {
   static final _databaseRefrence = FirebaseFirestore.instance;
 
   // collection refrences for better scalibility
 
   final CollectionReference users = _databaseRefrence.collection('users');
+  final CollectionReference rewards = _databaseRefrence.collection('rewards');
 }

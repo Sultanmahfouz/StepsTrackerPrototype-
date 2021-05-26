75af677360a3f1701ca9bcc8c2fe711c8bebe304
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-26 12:14:52 +0300

    main competition for providing the list of data

---
+++ lib/screens/competition/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:steps_tracker_prototype/models/user.dart';
+import 'package:steps_tracker_prototype/screens/competition/competition.dart';
+
+class MainCompetition extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final allUsers = Provider.of<List<UserData>>(context);
+    return Scaffold(
+      body: Competitions(
+        allUsers: allUsers,
+      ),
+    );
+  }
+}

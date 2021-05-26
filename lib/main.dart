0774157d198cbddbbc6cfe6d3bbbdaa40b291042
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-26 10:45:36 +0300

    include notifier in the main root of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/rewards/rewards.dart';
+import 'package:steps_tracker_prototype/models/user.dart';
+import 'package:steps_tracker_prototype/screens/authentication/main_auth.dart';
+import 'package:steps_tracker_prototype/services/reward.dart';
+import 'package:steps_tracker_prototype/services/user.dart';
+import 'package:steps_tracker_prototype/start.dart';
+import 'Theme/theme_provider.dart';
+import 'models/reward.dart';
+import 'package:provider/provider.dart';
+
+import 'screens/authentication/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,13 +18,44 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Rewards(),
+    return ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      builder: (context, _) {
+        final themeProvider = Provider.of<ThemeProvider>(context);
+        return StreamProvider<UserData>.value(
+          value: UserService().user,
+          child: StreamProvider<List<Reward>>.value(
+            value: RewardsService().getAvailableRewards(),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              themeMode: themeProvider.themeMode,
+              theme: MyTheme.lightTheme,
+              darkTheme: MyTheme.darkTheme,
+              initialRoute: FirebaseAuth.instance.currentUser?.uid == null ||
+                      FirebaseAuth.instance.currentUser?.isAnonymous == true
+                  ? MainAuthendication.id
+                  : StartApp.id,
+              routes: {
+                MainAuthendication.id: (context) => MainAuthendication(),
+                Login.id: (context) => Login(),
+                StartApp.id: (context) => StartApp(),
+              },
+              home: FirebaseAuth.instance.currentUser?.uid == null
+                  ? MainAuthendication()
+                  : StartApp(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/start.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:steps_tracker_prototype/services/user.dart';
+
+import 'Theme/theme_provider.dart';
+import 'models/user.dart';
+import 'screens/landing.dart';
+
+class StartApp extends StatelessWidget {
+  static String id = 'start_screen';
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<UserData>.value(
+      value:
+          UserService().getUserDocument(FirebaseAuth.instance.currentUser?.uid),
+      child: ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            themeMode: themeProvider.themeMode,
+            theme: MyTheme.lightTheme,
+            darkTheme: MyTheme.darkTheme,
+            home: Landing(),
+          );
+        },
+      ),
+    );
+  }
+}

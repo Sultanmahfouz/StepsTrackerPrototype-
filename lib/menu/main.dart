574a62efe14094f58aacd39a0cda50d92b869f5a
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-26 10:53:06 +0300

    main menu tree

---
+++ lib/menu/main.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:steps_tracker_prototype/models/user.dart';
+import 'package:steps_tracker_prototype/services/user.dart';
+
+import 'drawer.dart';
+
+class MainMenu extends StatelessWidget {
+  List<bool> isHighlighted;
+  MainMenu(this.isHighlighted);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<UserData>.value(
+      value:
+          UserService().getUserDocument(FirebaseAuth.instance.currentUser?.uid),
+      child: MyDrawer(isHighlighted),
+    );
+  }
+}

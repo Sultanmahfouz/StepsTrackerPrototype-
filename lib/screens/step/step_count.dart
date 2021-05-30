d51cad981e7472aad8a23e0a4796a44cf6970f1f
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-30 13:48:22 +0300

    include step counting in the background

--- lib/screens/step/step_count.dart
+++ lib/screens/step/step_count.dart
@@ -1,11 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:pedometer/pedometer.dart';
+
 import 'package:steps_tracker_prototype/components/constants.dart';
 import 'package:steps_tracker_prototype/menu/drawer.dart';
-import 'package:steps_tracker_prototype/services/user.dart';
 import 'package:intl/intl.dart';
+import 'package:steps_tracker_prototype/services/step.dart';
 
 class StepCounter extends StatefulWidget {
   @override
@@ -13,70 +11,7 @@ class StepCounter extends StatefulWidget {
 }
 
 class _StepCounterState extends State<StepCounter> {
-  Stream<StepCount> _stepCountStream;
-
-  String _steps = '?';
-
-  bool initial = true;
-  int currentStep = 0;
-  int initialStep = 0;
-  bool isHealthPoint = false;
-  bool exchangePointsOccured = false;
   List<bool> isHighlighted = [false, false, false, true, false, false];
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  void onStepCount(StepCount event) {
-    setState(() {
-      if (initial) {
-        initialStep = event.steps;
-        initial = false;
-      }
-      currentStep = event.steps;
-      _steps = event.steps.toString();
-      UserService(uid: FirebaseAuth.instance.currentUser.uid)
-          .updateUserDataSteps(currentStep.toString());
-
-      exchangePointsOccured = exchangePoints();
-
-      if (exchangePointsOccured) {
-        setState(() {
-          initialStep = currentStep;
-        });
-      }
-    });
-  }
-
-  // exchange footsteps with health points that can be user for rewards
-  bool exchangePoints() {
-    allStepsTaken = currentStep - initialStep;
-
-    // assuming the person has a taget of 10,000 steps a day not more
-    if (allStepsTaken > 10 && healthPoints <= 1000) {
-      healthPoints += 1;
-      allStepsTaken -= 10;
-      return true;
-    }
-    return false;
-  }
-
-  void onStepCountError(error) {
-    print('onStepCountError: $error');
-    setState(() {
-      _steps = 'Step Count is not available';
-    });
-  }
-
-  void initPlatformState() {
-    _stepCountStream = Pedometer.stepCountStream;
-    _stepCountStream.listen(onStepCount).onError(onStepCountError);
-
-    if (!mounted) return;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -116,7 +51,7 @@ class _StepCounterState extends State<StepCounter> {
               ),
               Center(
                 child: Text(
-                  _steps,
+                  StepCounting.steps,
                   style: TextStyle(
                     fontSize: 30,
                     color: Theme.of(context).primaryColor,
@@ -143,7 +78,7 @@ class _StepCounterState extends State<StepCounter> {
                     style: TextStyle(color: Colors.white),
                   ),
                   Text(
-                    currentStep.toString(),
+                    StepCounting.currentStep.toString(),
                     style: TextStyle(fontSize: 40, color: Colors.white),
                   ),
                 ],
@@ -156,7 +91,7 @@ class _StepCounterState extends State<StepCounter> {
                     style: TextStyle(color: Colors.white),
                   ),
                   Text(
-                    initialStep.toString(),
+                    StepCounting.initialStep.toString(),
                     style: TextStyle(fontSize: 40, color: Colors.white),
                   ),
                 ],

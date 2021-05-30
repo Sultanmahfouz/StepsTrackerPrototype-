53caa32934bdd35a4d2d5702331a5eaaedb44215
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-05-30 13:49:39 +0300

    notification is functional for both platforms ✅

--- lib/screens/landing/landing.dart
+++ lib/screens/landing/landing.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:steps_tracker_prototype/components/constants.dart';
 import 'package:steps_tracker_prototype/menu/main.dart';
 import 'package:steps_tracker_prototype/models/user.dart';
-import 'package:steps_tracker_prototype/screens/rewards/main.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class Landing extends StatefulWidget {
   @override
@@ -14,10 +15,77 @@ class _LandingState extends State<Landing> {
   List<bool> isHighlighted = [true, false, false, false, false, false];
   String username;
 
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
+
+  InitializationSettings initilizationSettings;
+
+  void _showNotification() async {
+    await _demoNotification();
+  }
+
+  Future<void> _demoNotification() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'channel_ID', 'channel name', 'channel description',
+        importance: Importance.Max,
+        priority: Priority.High,
+        ticker: 'test ticker');
+
+    var iOSChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+        0, 'دعاء', 'لا اله الا الله وحده لا شريك له', platformChannelSpecifics,
+        payload: 'test oayload');
+  }
+
   @override
   void initState() {
-    username = 'guest';
     super.initState();
+    username = 'guest';
+    var initilizationSettingsAndroid =
+        new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initilizationSettingsIOS = new IOSInitializationSettings(
+        requestAlertPermission: true,
+        requestBadgePermission: true,
+        requestSoundPermission: true,
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    initilizationSettings = new InitializationSettings(
+      initilizationSettingsAndroid,
+      initilizationSettingsIOS,
+    );
+    flutterLocalNotificationsPlugin.initialize(initilizationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  Future onSelectNotification(String payload) async {
+    if (payload != null) {
+      debugPrint('Notification payload: $payload');
+    }
+    await Navigator.push(context,
+        new MaterialPageRoute(builder: (context) => new SecondRoute()));
+  }
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+              title: Text(title),
+              content: Text(body),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text('Ok'),
+                  onPressed: () async {
+                    Navigator.of(context, rootNavigator: true).pop();
+                    await Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SecondRoute()));
+                  },
+                )
+              ],
+            ));
   }
 
   @override
@@ -51,103 +119,110 @@ class _LandingState extends State<Landing> {
               ],
             ),
           ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                child: Image.asset('assets/images/header.jpeg'),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: isAnon
-                          ? AssetImage('assets/images/anonymous.png')
-                          : user?.avatar == null || user?.avatar == ''
-                              ? AssetImage('assets/images/anonymous.png')
-                              : NetworkImage(user?.avatar),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(left: 14),
-                      child: Text(
-                        'Hey ',
-                        style: TextStyle(
-                          fontSize: 20,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  child: Image.asset('assets/images/header.jpeg'),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: isAnon
+                            ? AssetImage('assets/images/anonymous.png')
+                            : user?.avatar == null || user?.avatar == ''
+                                ? AssetImage('assets/images/anonymous.png')
+                                : NetworkImage(user?.avatar),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left: 14),
+                        child: Text(
+                          'Hey ',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      child: Text(
-                        username.length > 15
-                            ? username.substring(0, 15) + '..!'
-                            : username + ' !',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                      Container(
+                        child: Text(
+                          username.length > 15
+                              ? username.substring(0, 15) + '..!'
+                              : username + ' !',
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                    // Text('Hey ${username}'),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 100,
-              ),
-              Center(
-                child: Container(
-                  height: 40,
-                  child: Text(
-                    'Welcome to Healthy App',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
+                      // Text('Hey ${username}'),
+                    ],
                   ),
                 ),
-              ),
-              Divider(
-                height: 10,
-                thickness: 4,
-                indent: 150,
-                endIndent: 150,
-                color: Theme.of(context).primaryColor,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 50, horizontal: 100),
-                // ignore: deprecated_member_use
-                child: RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Colors.grey),
-                  ),
-                  elevation: 4,
-                  color: Theme.of(context).primaryColor,
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => MainReward()),
-                    );
-                  },
-                  child: Center(
+                SizedBox(
+                  height: 100,
+                ),
+                Center(
+                  child: Container(
+                    height: 40,
                     child: Text(
-                      'START',
+                      'Welcome to Healthy App',
                       style: TextStyle(
-                        color: Colors.white,
-                        letterSpacing: 1.5,
-                        fontWeight: FontWeight.bold,
                         fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Divider(
+                  height: 10,
+                  thickness: 4,
+                  indent: 150,
+                  endIndent: 150,
+                  color: Theme.of(context).primaryColor,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 50, horizontal: 100),
+                  // ignore: deprecated_member_use
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.grey),
+                    ),
+                    elevation: 4,
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () {
+                      _showNotification();
+                      print('should show notification');
+                    },
+                    // onPressed: () {
+                    //   Navigator.pushReplacement(
+                    //     context,
+                    //     MaterialPageRoute(builder: (context) => MainReward()),
+                    //   );
+                    // },
+                    child: Center(
+                      child: Text(
+                        'START',
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 1.5,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       );
@@ -158,5 +233,24 @@ class _LandingState extends State<Landing> {
         ),
       );
     }
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('route 2'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: Text('Go Back ...'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/start.dart
+++ lib/start.dart
@@ -1,14 +1,86 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pedometer/pedometer.dart';
 import 'package:provider/provider.dart';
 import 'package:steps_tracker_prototype/screens/landing/landing.dart';
+import 'package:steps_tracker_prototype/services/step.dart';
 import 'package:steps_tracker_prototype/services/user.dart';
 
 import 'Theme/theme_provider.dart';
+import 'components/constants.dart';
 import 'models/user.dart';
 
-class StartApp extends StatelessWidget {
+class StartApp extends StatefulWidget {
   static String id = 'start_screen';
+
+  @override
+  _StartAppState createState() => _StartAppState();
+}
+
+class _StartAppState extends State<StartApp> {
+  Stream<StepCount> _stepCountStream;
+
+  bool initial = true;
+  int currentStep = 0;
+  int initialStep = 0;
+  bool isHealthPoint = false;
+  bool exchangePointsOccured = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    setState(() {
+      if (initial) {
+        initialStep = event.steps;
+        initial = false;
+      }
+      currentStep = event.steps;
+      StepCounting.steps = event.steps.toString();
+
+      UserService(uid: FirebaseAuth.instance.currentUser.uid)
+          .updateUserDataSteps(currentStep.toString());
+
+      exchangePointsOccured = exchangePoints();
+
+      if (exchangePointsOccured) {
+        setState(() {
+          initialStep = currentStep;
+        });
+      }
+    });
+  }
+
+  // exchange footsteps with health points that can be user for rewards
+  bool exchangePoints() {
+    int allStepsTaken = currentStep - initialStep;
+
+    // assuming the person has a taget of 10,000 steps a day not more
+    if (allStepsTaken > 10 && healthPoints <= 1000) {
+      healthPoints += 1;
+      allStepsTaken -= 10;
+      return true;
+    }
+    return false;
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      StepCounting.steps = 'Step Count is not available';
+    });
+  }
+
+  void initPlatformState() {
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamProvider<List<UserData>>.value(
